b37963697c713c10cd9868a15fc03a92eecba27a
Jessica <jesss8692@hotmail.com>
2025-03-11 14:36:22 +0100

    Add project files

---
+++ src/components/hero.tsx
@@ -0,0 +1,12 @@
+function Hero() {
+
+  return (
+    <>
+        <h1>Find your deal</h1>
+        <p>Browse through great book deals and get you new favorite book.</p>
+        <button>Go to Deals</button>
+    </>
+  )
+}
+
+export default Hero
